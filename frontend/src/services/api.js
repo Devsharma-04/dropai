d2ab50964d33cd5ec58fe0cd5ff9383f09367468
dev <devsharma06658@gmail.com>
2026-07-06 02:33:57 +0530

    Updated code and fixed deployment settings

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API BASE URL =", "https://dropai-1-geqa.onrender.com/api/");
+
 const api = axios.create({
   baseURL: "https://dropai-1-geqa.onrender.com/api/",
   headers: {
