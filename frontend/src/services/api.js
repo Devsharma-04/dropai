fc82c61f7587f0f0e75173aea5954e38d5806b41
dev <devsharma06658@gmail.com>
2026-07-06 12:41:45 +0530

    Final URL change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dropai-1-geqa.onrender.com/",
+  baseURL: "https://dropai-1-geqa.onrender.com/api/", // Prefix `/api/` added
   headers: {
     "Content-Type": "application/json",
   },
