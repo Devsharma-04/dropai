188b7580711c6178bf6e9e624692fe22fbe2275c
dev <devsharma06658@gmail.com>
2026-07-06 12:02:11 +0530

    Force fix: Finalizing Backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,9 @@
-// src/services/api.js
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  // Purana URL hata kar ye NAYA URL daalo:
-  baseURL: 'https://dropai-1-geqa.onrender.com/', 
+  baseURL: "https://dropai-1-geqa.onrender.com/",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
